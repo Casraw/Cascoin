327eb344660882f7a67a1b900a2183ed2ff32eb6
Alexander Bergmann <casraw@protonmail.com>
2025-11-07 22:25:01 +0100

    evm dep

--- depends/packages/evmc.mk
+++ depends/packages/evmc.mk
@@ -6,25 +6,24 @@ $(package)_sha256_hash=0d5458015bf38a5358fad04cc290d21ec40122d1eb6420e0b33ae2554
 $(package)_dependencies=
 
 define $(package)_set_vars
-  $(package)_config_opts=-DCMAKE_BUILD_TYPE=Release
+  $(package)_config_opts=-DCMAKE_INSTALL_PREFIX=$(host_prefix)
+  $(package)_config_opts+=-DCMAKE_BUILD_TYPE=Release
   $(package)_config_opts+=-DEVMC_TESTING=OFF
   $(package)_config_opts+=-DEVMC_TOOLS=OFF
   $(package)_config_opts+=-DEVMC_EXAMPLES=OFF
   $(package)_config_opts+=-DBUILD_SHARED_LIBS=OFF
-  $(package)_config_opts_release=-DCMAKE_BUILD_TYPE=Release
-  $(package)_config_opts_debug=-DCMAKE_BUILD_TYPE=Debug
 endef
 
 define $(package)_config_cmds
-  $($(package)_cmake) .
+  mkdir -p build && cd build && cmake .. $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  cd build && $(MAKE)
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  cd build && $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
 
 define $(package)_postprocess_cmds
--- depends/packages/evmone.mk
+++ depends/packages/evmone.mk
@@ -6,24 +6,35 @@ $(package)_sha256_hash=e9f1f43c82d8b5675b7e8b2c0ce7ffba808508475ebf59c9dc9700c33
 $(package)_dependencies=evmc
 
 define $(package)_set_vars
-  $(package)_config_opts=-DCMAKE_BUILD_TYPE=Release
+  $(package)_config_opts=-DCMAKE_INSTALL_PREFIX=$(host_prefix)
+  $(package)_config_opts+=-DCMAKE_BUILD_TYPE=Release
   $(package)_config_opts+=-DEVMONE_TESTING=OFF
   $(package)_config_opts+=-DEVMONE_FUZZING=OFF
   $(package)_config_opts+=-DBUILD_SHARED_LIBS=OFF
-  $(package)_config_opts_release=-DCMAKE_BUILD_TYPE=Release
-  $(package)_config_opts_debug=-DCMAKE_BUILD_TYPE=Debug
+  $(package)_config_opts+=-DEVMC_ROOT=$(host_prefix)
+endef
+
+define $(package)_preprocess_cmds
+  rm -rf evmc && \
+  git init && \
+  git config user.email "build@cascoin.local" && \
+  git config user.name "Cascoin Build" && \
+  git add . && \
+  git commit -m "Initial" && \
+  git submodule add https://github.com/ethereum/evmc.git evmc && \
+  cd evmc && git checkout v$(evmc_version) && cd ..
 endef
 
 define $(package)_config_cmds
-  $($(package)_cmake) .
+  mkdir -p build && cd build && cmake .. $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  cd build && $(MAKE)
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  cd build && $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
 
 define $(package)_postprocess_cmds
